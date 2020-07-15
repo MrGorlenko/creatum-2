6f3b8fa82806d6e738c73f08a7d0d6a1905015cb
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-07-15 16:55:18 +0500

    something to fix

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,44 @@
 $(document).ready(function(){
   let controller = new ScrollMagic.Controller();
 
-  let ourScene = new ScrollMagic.Scene({
+  let navWhite = new ScrollMagic.Scene({
+    triggerElement: '#about',
+    triggerHook: .7
+  })
+  .setClassToggle('#nav', 'nav-white')
+  // .addIndicators()
+  .addTo(controller);
+  
+  let toggleWhite = new ScrollMagic.Scene({
+    triggerElement: '#about',
+    triggerHook: .7
+  })
+  .setClassToggle('.nav-span', 'span-white')
+  // .addIndicators()
+  .addTo(controller);
+
+  let LogoWhite = new ScrollMagic.Scene({
+    triggerElement: '#about',
+    triggerHook: 0.7,
+  })
+  .setClassToggle('#logo', 'logo-white')
+  // .addIndicators()
+  .addTo(controller);
+
+  let ourScene1 = new ScrollMagic.Scene({
     triggerElement: '#about',
     triggerHook: 0.9
   })
   .setClassToggle('#circle', 'wider')
+  // .addIndicators()
+  .addTo(controller);
+
+  let ourScene2 = new ScrollMagic.Scene({
+    triggerElement: '#about',
+    triggerHook: 0.5
+  })
+
+  .setClassToggle('#line', 'full-line')
   // .addIndicators()
   .addTo(controller);
 })
