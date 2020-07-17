73a445972ba7a0df88e63824e2b098993231a319
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-07-17 17:17:01 +0500

    yoba slider

--- js/slider-right.js
+++ js/slider-right.js
@@ -1,28 +1,17 @@
 $(document).ready(function(){
-  TweenLite.defaultEase = Linear.easeNone;
-  // var containers = document.querySelectorAll(".slider-container");
-  let containers = $('.container-xl');
-  var controller = new ScrollMagic.Controller();
-  var tl = new TimelineMax();
 
-  tl.to("#js-slideContainer", 1, {xPercent: -20}, "label1");
-  tl.from(containers[1], 0.5, {opacity:0}, "label1+=0.5");
-  tl.to("#js-slideContainer", 1, {xPercent: -40}, "label2");
-  tl.from(containers[2], 0.5, {opacity:0}, "label2+=0.5");
-  tl.to("#js-slideContainer", 1, {xPercent: -50}, "label3");
+  let container = document.getElementById('swiperContainer')
+  let swiper = new Swiper( container, {
+    speed: 400,
+    direction: 'horizontal',
+    touchReleaseOnEdges: true,
+    mousewheel: true,
+    loop: false
+  });
 
-  new ScrollMagic.Scene({
-    triggerElement: "#js-wrapper",
-    triggerHook: "onLeave",
-    duration: "400%"
-  })
-    .setPin("#js-wrapper")
-    .setTween(tl)
-    // .addIndicators({
-    //   colorTrigger: "white",
-    //   colorStart: "white",
-    //   colorEnd: "white",
-    // })
-    .addTo(controller);
+
+  swiper.on('reachEnd', function(){
+      swiper.mousewheel.disable()
+    })
 
 })
